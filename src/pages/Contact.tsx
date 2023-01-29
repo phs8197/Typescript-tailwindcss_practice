b39ce147410d96b62219f9409027c68e0296db1d
phs8197 <pjk1245@naver.com>
2023-01-29 22:10:49 +0900

    add contact text

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ContactImage from "../img/02.png";
+import Contact_cont from "./Contact_cont";
 
 function Contact() {
   return (
     <div className="h-screen bg-secondary">
       <div className="columns-2">
         <img src={ContactImage} alt="" />
-        <div></div>
+        <div className="mt-30">
+          <Contact_cont></Contact_cont>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Contact_cont.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Contact_cont() {
+  return (
+    <div>
+      <div className="text-3xl font-bold">1. 프릳츠 도화</div>
+      <div className="text-xl font-normal">
+        전화번호. 02-3275-2045
+        <br />
+        주소. 서울시 마포구 새창로 2길 17
+        <br />
+        영업시간. 평일 오전 8시 - 오후 10시
+        <br />
+        (주말, 공휴일 오전 10시 - 오후 10시)
+      </div>
+    </div>
+  );
+}
+
+export default Contact_cont;
